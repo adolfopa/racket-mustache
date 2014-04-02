48bd703fa7f54822fa96000aebc28c4975a0c6f7
Adolfo Perez Alvarez <adolfo.pa@gmail.com>
2014-04-02 21:30:42 +0200

    add tests for inverted sections and rename sections test files

--- mustache/tests/template-test.rkt
+++ mustache/tests/template-test.rkt
@@ -2,8 +2,9 @@
 
 (require (prefix-in only-text: "only-text.ms")
          (prefix-in simple-ref: "simple-ref.ms")
-         (prefix-in sequence: "sequence.ms")
-         (prefix-in sequence-ref: "sequence-ref.ms"))
+         (prefix-in section: "section.ms")
+         (prefix-in section-ref: "section-ref.ms")
+         (prefix-in inversion: "inversion.ms"))
 
 (module+ test
   (require rackunit))
@@ -19,9 +20,13 @@
   (check-tpl simple-ref:render (hash "foo" "bar") "The variable value is bar.\n")
   (check-tpl simple-ref:render (hash) "The variable value is .\n")
   
-  (check-tpl sequence:render (hash "foo" #t) "Here are some X.\n")
-  (check-tpl sequence:render (hash "foo" (list 1 2 3)) "Here are some XXX.\n")
-  (check-tpl sequence:render (hash "foo" #f) "Here are some .\n")
-  (check-tpl sequence:render (hash) "Here are some .\n")
+  (check-tpl section:render (hash "foo" #t) "Here are some X.\n")
+  (check-tpl section:render (hash "foo" (list 1 2 3)) "Here are some XXX.\n")
+  (check-tpl section:render (hash "foo" #f) "Here are some .\n")
+  (check-tpl section:render (hash) "Here are some .\n")
   
-  (check-tpl sequence-ref:render (hash "foo" (list (hash "bar" 42))) "The answer is 42.\n"))
+  (check-tpl section-ref:render (hash "foo" (list (hash "bar" 42))) "The answer is 42.\n")
+  
+  (check-tpl inversion:render (hash "foo" "xyz") "\n")
+  (check-tpl inversion:render (hash "foo" #f) "Maybe ...\n")
+  (check-tpl inversion:render (hash) "Maybe ...\n"))
